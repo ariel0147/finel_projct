a340efd76a0debfaf0b56e8bd920f951a51c9321
ariel0147 <arial055665@gmail.com>
2025-10-03 14:54:35 +0300

    Creating a Crit Grid function creates the information from the server in the form of a card

--- public/script.js
+++ public/script.js
@@ -14,3 +14,25 @@ async function getData(){
         alert(err);
     }
 }
+function createGrid(data){
+    let txt = "";
+    for(obj of data){
+        if(obj){
+            txt +=
+                `<div class="card">
+                    <div>
+                        <img src="../images/${obj.myFileName}?t=${Date.now()}" alt="${obj.name}">
+                        <p>${obj.name}</p>
+                        <div>${obj.Description}</div>
+                        <div>👍🏻 ${obj.likes ?? 0} | 👎🏻 ${obj.dislikes ?? 0}</div>
+                    </div>
+                    <div>
+                        <button onclick="deleteProduct(${obj.id})">Delete</button>
+                        <button onclick="getById(${obj.id})">Edit</button>
+                        <button onclick="ProjectView(${obj.id})">Project-View</button>
+                    </div>
+                </div>`
+        }
+    }
+    document.getElementById('main').innerHTML = txt;
+}

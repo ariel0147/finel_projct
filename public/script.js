db8449d4ab98f58d36c489f3d7956f5a2b3c8d04
ariel0147 <arial055665@gmail.com>
2025-10-03 14:53:20 +0300

    Creating a GetData function that generates the information from the server

---
+++ public/script.js
@@ -0,0 +1,16 @@
+function addTitle(){
+    let txt = "project";
+    document.getElementById("h1").innerText = txt;
+}
+async function getData(){
+
+    try {
+        let response = await fetch('/p');
+        let data = await response.json();
+
+        createGrid(data);
+
+    }catch(err){
+        alert(err);
+    }
+}
